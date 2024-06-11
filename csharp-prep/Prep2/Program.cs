1341259725a0821c1dd3bc8023077c11ca733265
CodyCraner <cra23035@byui.edu>
2024-06-11 14:47:02 -0600

    Completed prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade: ");
+        int grade = 0;
+        grade = Int32.Parse(Console.ReadLine());
+        //Obtained from stack overflow
+        string letterGrade = "Unknown";
+        Boolean passed = false;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+            passed = true;
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+            passed = true;
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+            passed = true;
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"You have a {letterGrade}");  
+        if (passed == true)
+        {
+            Console.WriteLine("You Passed!");
+
+        }
+        else if (passed == false)
+        {
+            Console.WriteLine("You did not pass.");
+        }
+
     }
 }
